d1b04d171aeba22e0f376695799e2884775bb8a4
EdiYulianto12 <84266203+EdiYulianto12@users.noreply.github.com>
2021-07-11 19:46:30 +0700

    Tugas Uas

---
+++ about.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Tabel daftar</title>
+   <style type="text/css">
+    .color{
+      background-color: rgb(107, 229, 210);
+    }
+  </style>
+ <link rel="stylesheet" type="text/css" href="style.css"><meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+footer {
+  text-align: center;
+ height: 50px;
+  background-color: #414949;
+  color: white;
+  font-size: 20px;
+}
+.abot{
+  width: auto;
+  height: auto;
+ 
+  align-content: center;
+  padding: 40px;
+
+</style>
+  </head>
+<body>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+       <li class="nav-item">
+       <a class="nav-link" href="pusat/anggota/daftar.php">Anggota</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pusat/pustaka/daftar.php">Pustaka</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="pusat/pustakawan/daftar.php">Pustakawan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=" pusat/transaksi/daftar.php">Transaksi</a>
+      </li> 
+       <li class="nav-item">
+        <a class="nav-link" href=" about.php">About</a>
+      </li>
+    </ul>
+  </div>  
+</nav>
+<br>
+  
+<div class="abot">
+  <font color="black">
+    <center><h3>About</h3><br><br></center>
+<img src="img/foto.jpg" width="250" height="400" border="5">
+<br><h4>Nama  : Ryan Rahmat Kurniawan<br>
+        NPM   : 1955201102</h4><br><br>
+        <p class="misi">"Penghargaan paling tinggi bagi seorang pekerja keras bukanlah apa yang dia peroleh dari pekerjaan itu, tapi seberapa berkembang ia dengan kerja kerasnya itu." </p>
+        <p>Motto Hidup      : Siapa yang bersungguh-sungguh maka ia akan berhasil, dan berbahagialah dengan caramu sendiri bukan karna perkataan orang lain</p></font>
+
+</div>
+
+</div>
+<br><br><br>
+
+<footer>
+  <p>2021 |> Ryan Rahmat Kurniawan<</p><br>
+ 
+</footer>
+
+
+
+</body>
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Landing</title>
+   <style type="text/css">
+    .color{
+      background-color: rgb(107, 229, 210);
+    }
+  </style>
+ <link rel="stylesheet" type="text/css" href="style.css"><meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+  <style>
+footer {
+  text-align: center;
+ height: 100px;
+  background-color: #414949;
+  color: white;
+  font-size: 20px;
+}
+.mail{
+  text-align: left;
+}
+.bord{
+  background-color: lightgreen;
+}
+
+
+.come{
+  text-align: center;
+  font-size: 50px;
+  font-family: sans-serif;
+
+}
+
+</style>
+  </head>
+<body>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="index.php">Home</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+       <li class="nav-item">
+       <a class="nav-link" href="pusat/anggota/daftar.php">Anggota</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="pusat/pustaka/daftar.php">Pustaka</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="pusat/pustakawan/daftar.php">Pustakawan</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href=" pusat/transaksi/daftar.php">Transaksi</a>
+      </li> 
+      <li class="nav-item">
+        <a class="nav-link" href="about.php">About</a>
+      </li>
+    </ul>
+  </div>  
+</nav>
+<br>
+  <br>
+<div class="come">Selamat Datang Di Aplikasi Di Perpustakaan</div>
+<br>
+<table cellpadding="10" cellspacing="10" align="center" class="bord">
+  <tr>
+    <td>
+      <a href="pusat/anggota/daftar.php"><img src="img/anggota.png" width="150" height="150">
+        <br><a href="pusat/anggota/daftar.php"><center>Anggota</a>
+      </td>
+
+      <td>
+        <a href="pusat/pustaka/daftar.php"><img src="img/pustaka.jpg" width="150" height="150">
+        <br>
+        <a href="pusat/pustaka/daftar.php"><center>Pustaka</a></td>
+        <td>
+        <a href="pusat/pustakawan/daftar.php"><img src="img/pustakawan.jpg" width="200" height="150">
+        <br><a href="pusat/pustakawan/daftar.php"><center>Pustakawan</a>
+        </td>
+        <td>
+        <a href="pusat/transaksi/daftar.php"><img src="img/transaksi.jpg" width="150" height="150">
+            <br><a href="pusat/transaksi/daftar.php"><center>Transaksi</a>
+            </td>
+  </tr>
+</table>
+
+
+
+<br><br><br><br>
+
+</div>
+<br><br><br>
+
+<footer>
+  <p>@2021 | Ryan Rahmat Kurniawan</p>
+ <p class="mail">Email : ryanrahmatkurniawan17@gmail.com</p>
+</footer>
+
+
+
+</body>
+</html>
